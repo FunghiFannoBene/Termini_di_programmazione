25b6f8f66ea0e2dabbe8b14c86a60399d3e937d5
FunghiFannoBene <119907315+FunghiFannoBene@users.noreply.github.com>
2023-07-16 01:24:36 +0200

    Create compiler_interpreted.c

---
+++ compiler_interpreted.c
@@ -0,0 +1,5 @@
+Compiler vs interpreted:
+
+https://www.learncpp.com/images/CppTutorial/Chapter0/Compiling-min.png?ezimgfmt=rs:521x161/rscb2/ng:webp/ngcb2
+
+https://www.learncpp.com/images/CppTutorial/Chapter0/Interpreting-min.png?ezimgfmt=rs:721x61/rscb2/ng:webp/ngcb2
